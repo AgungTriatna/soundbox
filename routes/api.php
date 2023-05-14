c951555bc6195d00a10077498441b35b1f00afed
Dandi Lesmana <dandilesmana.if21@gmail.com>
2023-05-14 06:08:30 -0700

    update>

--- routes/api.php
+++ routes/api.php
@@ -40,6 +40,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
 
 });
+
+// ini buat playlist #aji 
+Route::post('create-playlist',[PlaylistController::class,'create_playlist']);
+
+// Route::post('/', 'App\Http\Controllers\PlaylistManagementController@create_playlist');
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
